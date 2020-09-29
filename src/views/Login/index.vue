db3ef8679a5c63278e42a39e574713062e1356b1
enFFF <244876853@qq.com>
2020-09-29 16:46:07 +0800

    构建项目

---
+++ src/views/Login/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="login">
+        这是主页
+    </div>
+</template>
+<script>
+export default {
+    name: 'login'
+};
+</script>
+<style lang="scss" scoped>
+#login{
+    height: 100vh;
+    background-color: #334444;
+}
+</style>
